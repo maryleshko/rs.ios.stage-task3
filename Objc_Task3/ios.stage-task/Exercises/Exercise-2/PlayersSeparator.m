614ffab65f3979b22912a6cbabcf698458865f43
Maryia Liashko <maryia.liashko@devexperts.com>
2024-11-04 10:45:13 +0100

    Added solution to PlayersSeparator

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,29 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    if (ratingArray.count < 3) {
+        return 0;
+    }
+
+    NSInteger potentialTeamsCount = 0;
+    NSInteger playersCount = ratingArray.count;
+
+    for (NSInteger firstIndex = 0; firstIndex < playersCount - 2; firstIndex++) {
+        for (NSInteger secondIndex = firstIndex + 1; secondIndex < playersCount - 1; secondIndex++) {
+            for (NSInteger thirdIndex = secondIndex + 1; thirdIndex < playersCount; thirdIndex++) {
+                int firstRating = ratingArray[firstIndex].intValue;
+                int secondRating = ratingArray[secondIndex].intValue;
+                int thirdRating = ratingArray[thirdIndex].intValue;
+
+                if ((firstRating < secondRating && secondRating < thirdRating)
+                    || (firstRating > secondRating && secondRating > thirdRating)) {
+                    potentialTeamsCount++;
+                }
+            }
+        }
+    }
+
+    return potentialTeamsCount;
 }
 
 @end
